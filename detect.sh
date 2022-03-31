72944855301cc9c90634dfbd3e91203db3405b9d
zeqinzheng <zeqinzheng@tencent.com>
2022-03-31 19:54:02 +0800

    feat: detect and format files

--- detect.sh
+++ detect.sh
@@ -1,16 +1,29 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 [[ -d "input" ]] || mkdir input
+[[ -d "formatted" ]] || mkdir formatted
 
-for path in $(git status -uall | grep --color='never' input/ | awk '{ print $1 }' | tr -d '[:blank:]'); do
-    if [ -r $path ]; then
+input="input"
+formatted="formatted"
+tab=$'\t'
+
+for path in $(git status -uall | grep --color='never' $input/ | awk '{ print $1 }' | tr -d '[:blank:]'); do
+    if [[ -r $path ]]; then
         date=$(ls -lD %F $path | awk '{ print $6 }' )
-        existed=$(ls -l input | grep $date | wc -l | awk '{ print $1 }')
-        if [ $existed -eq 1 ]; then
+        existed=$(ls -l $input | grep $date | wc -l | awk '{ print $1 }')
+        if [ $existed -eq 0 ]; then
             mv $path input/$date
+            path="$input/$date"
         else
-            existed=$((existed+1))
-            mv $path input/$date_$existed
+            # ensure no duplicate filename
+            while [[ -e "$input/${date}_${existed}" ]]; do
+                existed=$((existed+1))
+            done
+            latest_path="$input/${date}_${existed}"
+            mv $path $latest_path
+            path=$latest_path
         fi
+        dest_path="$formatted/$(basename $path)"
+        cat $path | sed -E -e "s/ *${tab}+( *${tab}*)*/${tab}/" > $dest_path
     fi
 done
