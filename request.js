2eb23083ddf0d5c7e3d2aeea0fe90635fbf76e8d
zeqinzheng <zeqinzheng@tencent.com>
2022-04-02 17:22:38 +0800

    test: workflow

--- request.js
+++ request.js
@@ -57,7 +57,8 @@ const request_url = 'https://quizlet.com/webapi/3.2/terms/save?_method=PUT';
         // await page.waitForSelector(btn_import+":not([disabled])");
         await page.click(btn_import);
       } catch(e) {
-        page.waitForSelector(text_area);
+        await page.waitForSelector(text_area);
+        console.log(page.content());
         throw e
       }
       const response = await page.waitForResponse(request_url);
